a923ed4870419647fd2c3252abcd908403b470bf
BeGieU <marcinoo890@gmail.com>
2019-02-14 17:19:08 +0100

    added displaying author, and added author's reference to written books

--- src/main/java/com/example/library/controllers/AuthorController.java
+++ src/main/java/com/example/library/controllers/AuthorController.java
@@ -4,9 +4,28 @@ Author: BeGieU
 Date: 04.02.2019
 */
 
+import com.example.library.services.AuthorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/author")
 public class AuthorController
 {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService)
+    {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/{authorId}/show")
+    public String showAuthor(Model model, @PathVariable Long authorId)
+    {
+        model.addAttribute(authorService.findById(authorId));
+        return "author/show";
+    }
 }
